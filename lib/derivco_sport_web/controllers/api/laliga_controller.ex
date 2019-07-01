825ed87a57b49b98078c306716625c51116ae287
Ruben Serena <ruben.serena@admanmedia.com>
2019-07-01 14:23:14 +0200

    remind me to specs

--- lib/derivco_sport_web/controllers/api/laliga_controller.ex
+++ lib/derivco_sport_web/controllers/api/laliga_controller.ex
@@ -14,16 +14,19 @@ defmodule DerivcoSportWeb.Api.LaLigaController do
     |> get_body(conn)
   end
 
+  # @spec view_render 
   defp view_render(data_csv) do
     #hay que ver cómo hacer un bucle para listar los datos en el html
     Phoenix.View.render(DerivcoSportWeb.Api.LaLigaView, "index.html", data: data_csv)
   end
 
+  # @spec get_body 
   defp get_body({:safe, data}, conn) do
     data
     |> response(conn)
   end
 
+  # @spec response 
   defp response(data, conn) do
     conn
     |> send_resp(200, data)
