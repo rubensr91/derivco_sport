958244c9fc1cc057c0996ddec17c43dc8e1e3c6c
rubensr91 <rubensere@gmail.com>
2019-07-02 23:43:23 +0200

    yassss

--- lib/derivco_sport_web/controllers/api/laliga_controller.ex
+++ lib/derivco_sport_web/controllers/api/laliga_controller.ex
@@ -24,66 +24,78 @@ defmodule DerivcoSportWeb.Api.LaLigaController do
   end
 
   @spec index(Plug.Conn.t(), any) :: Plug.Conn.t()
-
   def index(conn, params \\ []) do
     @csv
     |> read_file()
     |> filter_or_not_by_params(params)
-    |> Jason.encode!()
+    |> encode_file()
     |> response(conn)
   end
 
   # @spec read_file
-
   defp read_file(file) do
     case File.read(file) do
       {:ok, data} ->
-        data
-        |> CSV.parse_string()
-        |> Enum.map(fn [
-                         coma,
-                         div,
-                         season,
-                         date,
-                         hometeam,
-                         awayteam,
-                         fthg,
-                         ftag,
-                         ftr,
-                         hthg,
-                         htag,
-                         htr
-                       ] ->
-          %{
-            div: div,
-            season: season,
-            date: date,
-            hometeam: hometeam,
-            awayteam: awayteam,
-            fthg: fthg,
-            ftag: ftag,
-            ftr: ftr,
-            hthg: hthg,
-            htag: htag,
-            htr: htr
-          }
-        end)
+        {:ok,
+         data
+         |> CSV.parse_string()
+         |> Enum.map(fn [
+                          _coma,
+                          div,
+                          season,
+                          date,
+                          hometeam,
+                          awayteam,
+                          fthg,
+                          ftag,
+                          ftr,
+                          hthg,
+                          htag,
+                          htr
+                        ] ->
+           %{
+             div: div,
+             season: season,
+             date: date,
+             hometeam: hometeam,
+             awayteam: awayteam,
+             fthg: fthg,
+             ftag: ftag,
+             ftr: ftr,
+             hthg: hthg,
+             htag: htag,
+             htr: htr
+           }
+         end)}
 
-      {:error, :enoent} ->
+      {:error, _} ->
         {:error, "Error reading file"}
     end
   end
 
   # @spec filter_or_not_by_params
-  defp filter_or_not_by_params(file, params) when is_map(params) and map_size(params) > 0 do
-    Enum.filter(file, &(&1.season == params["season"]))
+  defp filter_or_not_by_params({:ok, file}, params)
+       when is_map(params) and map_size(params) > 0 do
+    {:ok, Enum.filter(file, &(&1.season == params["season"]))}
   end
 
-  defp filter_or_not_by_params(file, _params), do: file
+  defp filter_or_not_by_params({:ok, file}, _params) do
+    {:ok, file}
+  end
+
+  defp filter_or_not_by_params({:error, _reason} = error, _params), do: error
+
+  # @spec encode_file
+  defp encode_file({:ok, file}) do
+    {:ok,
+     file
+     |> Jason.encode!()}
+  end
+
+  defp encode_file({:error, _reason} = error), do: error
 
   # @spec response
-
-  defp response(data, conn) do
+  defp response({:ok, data}, conn) do
     Logger.info("Read file correctly!")
 
     conn
